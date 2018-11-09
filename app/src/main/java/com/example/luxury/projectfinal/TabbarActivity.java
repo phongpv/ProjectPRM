e7e72da7f1479b60b5367db7eab043ff0289c86e
thanghq <ggxxhay123@gmail.com>
2018-11-09 20:57:35 +0700

    Fix error
    Tắt đi bật lại là đc!

--- app/src/main/java/com/example/luxury/projectfinal/TabbarActivity.java
+++ app/src/main/java/com/example/luxury/projectfinal/TabbarActivity.java
@@ -1,9 +1,6 @@
 package com.example.luxury.projectfinal;
 
-import android.app.ActionBar;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -13,8 +10,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
