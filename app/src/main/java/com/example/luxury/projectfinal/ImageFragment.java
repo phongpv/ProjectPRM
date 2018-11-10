52ee07cc395665109a9f22784bcc6f2b1f707a8e
PhongPV <phamvanphong915@gmail.com>
2018-11-10 20:52:52 +0700

    Temp

--- app/src/main/java/com/example/luxury/projectfinal/ImageFragment.java
+++ app/src/main/java/com/example/luxury/projectfinal/ImageFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
@@ -30,7 +31,7 @@ public class ImageFragment extends Fragment {
 
 //    private OnFragmentInteractionListener mListener;
 
-    TextView textView;
+    ListView listImage;
 
     public ImageFragment() {
         // Required empty public constructor
@@ -68,8 +69,7 @@ public class ImageFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_image, container, false);
-        textView = (TextView) v.findViewById(R.id.textView);
-        textView.setText("This is image fragment");
+        listImage = (ListView) v.findViewById(R.id.listImage);
         return v;
     }
 
