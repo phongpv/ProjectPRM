9f7adc4a74acbeac2181aebfa60260d2acfd1432
sony1412 <roboconjvn@gmail.com>
2018-11-12 19:41:53 +0700

    Video done
    trước khi merge hay push thì ông phải commit change nhé. Như kiểu lưu thay đổi ý

---
+++ app/src/main/java/com/example/luxury/projectfinal/YoutubeVideo.java
@@ -0,0 +1,21 @@
+package com.example.luxury.projectfinal;
+
+public class YoutubeVideo {
+
+    String url;
+
+    public YoutubeVideo(String url) {
+        this.url = url;
+    }
+
+    public YoutubeVideo() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
